fb65aa28d0dbd87d480f21d4c9d6c68a9b1a54f7
Andre Dumas <54788297+Andreeb27@users.noreply.github.com>
2026-03-18 17:45:26 -0400

    Fix syntax errors in add and divide functions

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-def add(a, b)
-  return a + b
+def add(a, b):
+    return a + b
 
-def divide(a, b)
-  # Intentional Issue
-  return a / b;
+def divide(a, b):
+    # Intentional Issue
+    return a / b
